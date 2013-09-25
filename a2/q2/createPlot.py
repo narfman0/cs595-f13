2e90876d90b24adbf2236c2eec9fd6ab815556d3
Jon Robison <narfman0@yahoo.com>
2013-09-25 08:10:40 -0400

    -Add rest of expanded url memento counts

--- a2/q2/createPlot.py
+++ a2/q2/createPlot.py
@@ -9,10 +9,12 @@ with open(input, 'rU') as f:
 	with open('mementoCount.txt', 'w') as o:
 		with open('gnuplot.dat', 'w') as gnuplotfile:
 			for line in f:
-				uri = 'http://mementoproxy.cs.odu.edu/aggr/timemap/link/' + line.strip()
+				original = line.strip()
+				uri = 'http://mementoproxy.cs.odu.edu/aggr/timemap/link/' + original
 				r = requests.get(uri)
+				print(r.content)
 				count = str(len(re.findall(b'rel".*memento.*"',r.content)))
-				print('Found ' + count + ' mementos for uri: ' + uri)
-				o.write(line.strip() + ' ' + count + '\n')
+				print('Found ' + count + ' mementos for uri: ' + original)
+				o.write(original + ' ' + count + '\n')
 				gnuplotfile.write(count + '\n')
 		call(["gnuplot", "gnuplot.config"])
