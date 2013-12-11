61f3a6a69e3f609d42598c367e3d4c55260545cd
Jon Robison <narfman0@yahoo.com>
2013-12-11 00:47:06 -0500

    -Update table with sufficient data. Update driver. Needs tweaking.

--- a10/q1/getTitles.py
+++ a10/q1/getTitles.py
@@ -5,6 +5,7 @@ import urllib.request
 from urllib.parse import urlparse
 import feedparser
 import traceback
+import re
 
 DEFAULT_COUNT=100
 DEFAULT_SOURCES='http://musicliberation.blogspot.com/search?max-results=100','http://musicliberation.blogspot.com/search?updated-max=2013-02-26T21:19:00Z&max-results=100&start=25&by-date=false','http://musicliberation.blogspot.com/search?updated-max=2013-02-26T21:19:00Z&max-results=100&start=25&by-date=false','http://musicliberation.blogspot.com/search?updated-max=2012-02-14T13:33:00Z&max-results=100&start=79&by-date=false','http://musicliberation.blogspot.com/search?updated-max=2011-12-27T12:48:00Z&max-results=100&start=93&by-date=false','http://musicliberation.blogspot.com/search?updated-max=2011-10-20T13:05:00%2B01:00&max-results=100&start=116&by-date=false'
@@ -17,6 +18,13 @@ else:
     count=sys.argv[1]
     urls=sys.argv[2]
 
+def getwords(doc):
+  splitter=re.compile('\\W*')
+  doc=re.compile(r'<[^>]+>').sub('',doc)  
+  words=[s.lower() for s in splitter.split(doc) 
+          if len(s)>2 and len(s)<20]
+  return dict([(w,1) for w in words])
+
 def parse(link):
     response = urllib.request.urlopen(link)
     soup = BeautifulSoup(response.read())
@@ -27,19 +35,23 @@ def getEntries(max,urls):
   entries=[]
   for url in urls:
     soup=parse(url)
-    for entry in soup.findAll('h3'):
-      try:
-        title=str(list(list(entry.children)[1].children)[0])
-        if 'Reviewed by' not in title and title.strip() != '' and title not in entries:
-          entries.append(title)
-          if max <= len(entries):
-            return entries
-      except:
-        print('Error on entry: ' + str(entry))
+    for post in soup.findAll('div',{'class':'post hentry'}):
+      for entry in post.findAll('h3'):
+        try:
+          title=str(list(list(entry.children)[1].children)[0])
+          body=''
+          for bodyEle in post.findAll('div',{'style':'text-align: justify;'}):
+            body+=str(bodyEle)
+          if 'Reviewed by' not in title and title.strip() != '' and title not in entries:
+            entries.append((title,body,),)
+            if max <= len(entries):
+              return entries
+        except:
+          print('Error on entry: ' + str(entry))
   return entries
 
 entries=getEntries(100,urls)
 print('Found ' + str(len(entries)) + ' entries. Contents:\n' + str(entries))
 with open('titles.txt','w') as f:
-  for entry in entries:
-    f.write(entry + '\n')
+  for title,body in entries:
+    f.write(title + '|' + body.replace('\n','').replace('|','') + '||\n')
--- a10/q2/docclass.py
+++ a10/q2/docclass.py
@@ -1,10 +1,12 @@
+#from pysqlite2 import dbapi2 as sqlite
+from sqlite3 import dbapi2 as sqlite
 import re
 import math
-from pysqlite2 import dbapi2 as sqlite
 
 def getwords(doc):
   splitter=re.compile('\\W*')
-  print doc
+  ## Remove all the HTML tags
+  doc=re.compile(r'<[^>]+>').sub('',doc)  
   # Split the words by non-alpha characters
   words=[s.lower() for s in splitter.split(doc) 
           if len(s)>2 and len(s)<20]
@@ -18,14 +20,21 @@ class classifier:
     self.fc={}
     # Counts of documents in each category
     self.cc={}
+    ## extract features for classification
     self.getfeatures=getfeatures
     
   def setdb(self,dbfile):
-    self.con=sqlite.connect(dbfile)    
+    self.con=sqlite.connect(dbfile)
+    self.con.execute('create table if not exists rss(num, entry, feature, predicted, actual, cprob)')
     self.con.execute('create table if not exists fc(feature,category,count)')
     self.con.execute('create table if not exists cc(category,count)')
 
-
+  def insert (self,num, entry, feature, predicted, actual, cp):
+    self.con.execute("insert into rss values ('%s','%s', '%s', '%s','%s', '%s')"
+                     % (num, entry, feature, predicted, actual, cp))
+    self.con.commit()   
+    
+  ## Increase the count of a feature/category pair
   def incf(self,f,cat):
     count=self.fcount(f,cat)
     if count==0:
@@ -35,7 +44,8 @@ class classifier:
       self.con.execute(
         "update fc set count=%d where feature='%s' and category='%s'" 
         % (count+1,f,cat)) 
-  
+
+  ## The number of times a feature has appeared in a category
   def fcount(self,f,cat):
     res=self.con.execute(
       'select count from fc where feature="%s" and category="%s"'
@@ -43,6 +53,7 @@ class classifier:
     if res==None: return 0
     else: return float(res[0])
 
+  ## Increase the count of a category
   def incc(self,cat):
     count=self.catcount(cat)
     if count==0:
@@ -51,22 +62,30 @@ class classifier:
       self.con.execute("update cc set count=%d where category='%s'" 
                        % (count+1,cat))    
 
+  ## The number of items in a category
   def catcount(self,cat):
     res=self.con.execute('select count from cc where category="%s"'
                          %(cat)).fetchone()
     if res==None: return 0
     else: return float(res[0])
 
+  ## The list of all categories
   def categories(self):
     cur=self.con.execute('select category from cc');
     return [d[0] for d in cur]
 
+  ## The total number of items
   def totalcount(self):
     res=self.con.execute('select sum(count) from cc').fetchone();
     if res==None: return 0
     return res[0]
 
 
+  ## The train method takes an item(document) and a classification.
+  ## It uses the getfeatures function to the break the item into its
+  ## separate features. It then calls incf to increase the counts for
+  ## this classification for every feature. Finally, it increases
+  ## the total count for this classification.
   def train(self,item,cat):
     features=self.getfeatures(item)
     # Increment the count for every feature with this category
@@ -77,6 +96,11 @@ class classifier:
     self.incc(cat)
     self.con.commit()
 
+  ## Probability is a number between 0 and 1, indicating
+  ## the likelihood of an event. You calculate the probability of
+  ## a word in a particular category by dividing the number of
+  ## times the word appears in a document in that category
+  ## by the total number of documents in the category.
   def fprob(self,f,cat):
     if self.catcount(cat)==0: return 0
 
@@ -95,6 +119,9 @@ class classifier:
     # Calculate the weighted average
     bp=((weight*ap)+(totals*basicprob))/(weight+totals)
     return bp
+
+
+
 
 class naivebayes(classifier):
   
@@ -138,6 +165,9 @@ class naivebayes(classifier):
       if probs[cat]*self.getthreshold(best)>probs[best]: return default
     return best
 
+## This function will return the probability that an item with the
+## specified feature belongs in the specified category, assuming there
+## will be an equal number of items in each category.
 class fisherclassifier(classifier):
   def cprob(self,f,cat):
     # The frequency of this feature in this category    
@@ -152,6 +182,8 @@ class fisherclassifier(classifier):
     p=clf/(freqsum)
     
     return p
+
+  
   def fisherprob(self,item,cat):
     # Multiply all the probabilities together
     p=1
@@ -164,6 +196,8 @@ class fisherclassifier(classifier):
 
     # Use the inverse chi2 function to get a probability
     return self.invchi2(fscore,len(features)*2)
+
+  ## Inverse chi-squared function
   def invchi2(self,chi, df):
     m = chi / 2.0
     sum = term = math.exp(-m)
@@ -171,6 +205,7 @@ class fisherclassifier(classifier):
         term *= m / i
         sum += term
     return min(sum, 1.0)
+
   def __init__(self,getfeatures):
     classifier.__init__(self,getfeatures)
     self.minimums={}
@@ -181,21 +216,19 @@ class fisherclassifier(classifier):
   def getminimum(self,cat):
     if cat not in self.minimums: return 0
     return self.minimums[cat]
+  
   def classify(self,item,default=None):
     # Loop through looking for the best result
     best=default
     max=0.0
     for c in self.categories():
-      p=self.fisherprob(item,c)
+      try:
+        p=self.fisherprob(item,c)
+      except:
+        print('Error on item: ' + str(item) + ' category: ' + c)
+        continue
       # Make sure it exceeds its minimum
       if p>self.getminimum(c) and p>max:
         best=c
         max=p
     return best
-
-def sampletrain(cl):
-  cl.train('Nobody owns the water.','good')
-  cl.train('the quick rabbit jumps fences','good')
-  cl.train('buy pharmaceuticals now','bad')
-  cl.train('make quick money at the online casino','bad')
-  cl.train('the quick brown fox jumps','good')
